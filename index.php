39f6c4003011e95bb352ed322d9fe666bbfe642a
JonathonHu <hjyhujunyuan@hotmail.com>
2016-07-26 17:01:30 +0800

    开始写首页

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>首页</title>
+<style type="text/css">@import "./css/bootstrap.min.css";</style>
+</head>
+
+<body>
+<nav class="navbar navbar-inverse" role="navigation">
+   <div class="navbar-header">
+      <a class="navbar-brand" href="#">七毛小店</a>
+   </div>
+   <div>
+      <ul class="nav navbar-nav">
+         <li class="active"><a href="#">PHP</a></li>
+         <li><a href="#">Java</a></li>
+         <li><a href="#">Python</a></li>
+      </ul>
+   </div>
+</nav>
+<div class="container">
+<!--大框架
+	将页面分成两栏，左右比例为5：1；左边用来放置首页正文，右边往博客介绍，右边放日历、博客介绍等内容
+-->
+
+
+<!--以下是左部分-->
+<div class=" col-lg-10 col-sm-10">hello</div>
+
+<!--以下是右部分-->
+<div class="col-lg-2 col-sm-10">world</div>
+	<table>
+    <tr><td>导航一</td></tr>
+    <tr><td>导航二</td></tr>
+    <tr><td>导航三</td></tr>
+    <tr><td>导航四</td></tr>
+    </table>
+    </div>
+
+</div>
+</body>
+</html>
